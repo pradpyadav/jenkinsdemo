7d43463e8c2326b9055bd7f6701cd2a0f433cf05
Pradeep Yadav <pradpyadav@gmail.com>
2020-05-28 22:34:11 -0400

    Jenkin demo commit 1

---
+++ jenkindemo1/src/main/java/com/luv2code/jenkinproject/Jenkindemo1Application.java
@@ -0,0 +1,13 @@
+package com.luv2code.jenkinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkindemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkindemo1Application.class, args);
+	}
+
+}
