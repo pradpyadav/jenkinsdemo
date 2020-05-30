029336acbecb3f9c1fa69f9e2a1a421d990b5ab0
Pradeep Yadav <pradpyadav@gmail.com>
2020-05-30 18:04:35 -0400

    clumsy code

--- jenkindemo1/src/main/java/com/luv2code/jenkinproject/Jenkindemo1Application.java
+++ jenkindemo1/src/main/java/com/luv2code/jenkinproject/Jenkindemo1Application.java
@@ -9,8 +9,7 @@ public class Jenkindemo1Application {
 	int num1,num2;
 	public static void SayHello(String name) {
 		System.out.println("Hello  There : "+name);
-	}
-
+	}/*****/
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of the  Jenkins DevOps");
 		SpringApplication.run(Jenkindemo1Application.class, args);
