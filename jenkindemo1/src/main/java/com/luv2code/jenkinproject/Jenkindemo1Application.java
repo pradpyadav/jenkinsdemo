603045c6e802709c31e873d3c1fc70c7b87e6cf2
Pradeep Yadav <pradpyadav@gmail.com>
2020-05-30 11:24:37 -0400

    Added SOPstatement to main class

--- jenkindemo1/src/main/java/com/luv2code/jenkinproject/Jenkindemo1Application.java
+++ jenkindemo1/src/main/java/com/luv2code/jenkinproject/Jenkindemo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jenkindemo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the world of the  Jenkins DevOps");
 		SpringApplication.run(Jenkindemo1Application.class, args);
 	}
 
